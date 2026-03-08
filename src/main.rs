4d690f2f0f2d9cf3bd2a3b90ccbdd29047b5d593
Stefan Neamtu <sdneamtu@gmail.com>
2026-03-08 03:36:44 -0400

    feat: learning structs through rectangles

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod fibonacci_sequence;
 mod guessing_game;
 mod menu;
+mod rectangles;
 mod string_stuff;
 mod temp_conversion;
 mod twelve_days_christmas;
@@ -8,6 +9,7 @@ mod twelve_days_christmas;
 use fibonacci_sequence::fibonacci_sequence;
 use guessing_game::guessing_game;
 use menu::render_menu;
+use rectangles::rectangles;
 use string_stuff::string_stuff;
 use temp_conversion::temperature_conversion;
 use twelve_days_christmas::twelve_days_of_christmas;
@@ -19,6 +21,7 @@ fn main() {
         "Farenheit to Celsius and vice versa convertor",
         "The Guessing Game",
         "String stuff",
+        "Rectangles",
     ];
     let menu_selection = render_menu(&menu_items);
     match menu_selection {
@@ -27,6 +30,7 @@ fn main() {
         2 => temperature_conversion(),
         3 => guessing_game(),
         4 => string_stuff(),
+        5 => rectangles(),
         _ => println!("Please enter a valid selection."),
     }
 }
